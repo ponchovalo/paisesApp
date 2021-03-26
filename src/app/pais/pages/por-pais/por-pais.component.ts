c0a80a970c9eef3366f3c64a53de1f05e69ca6d4
Ildefonso Valenzuela <ponchovalo@outlook.com>
2021-03-26 09:06:43 -0600

    services

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent{
+
+  termino:string=''
+
+  constructor() { }
+
+  buscar(){
+    console.log(this.termino)
+  }
+
+
+}
